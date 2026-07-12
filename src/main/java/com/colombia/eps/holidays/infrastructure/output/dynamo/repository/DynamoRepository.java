7bbb82eade57c9130db391752be33f8ff89d480c
oscar alvarado <proyectos.oscar-alvarado@outlook.com>
2026-07-11 22:38:25 -0500

    se modifica a lista el procesamiento y se modifica el gitignore para omitir la carpeta.idea

--- src/main/java/com/colombia/eps/holidays/infrastructure/output/dynamo/repository/DynamoRepository.java
+++ src/main/java/com/colombia/eps/holidays/infrastructure/output/dynamo/repository/DynamoRepository.java
@@ -29,7 +29,7 @@ public class DynamoRepository {
         return queryResult.stream()
                 .parallel()
                 .flatMap(page -> page.items().stream())
-                .collect(Collectors.toList());
+                .toList();
     }
 
 
